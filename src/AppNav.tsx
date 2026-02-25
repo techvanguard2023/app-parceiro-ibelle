5c2b4a8d3ec6e974323f5264c302590e9898b3a9
Tech Vanguard <154639339+techvanguard2023@users.noreply.github.com>
2026-02-24 22:38:14 -0300

    feat: update App.tsx with navigation

---
+++ src/AppNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { RootNavigator } from './navigation';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <RootNavigator />
+    </GestureHandlerRootView>
+  );
+}
